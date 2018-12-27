8535e44713b0bee26dff0711b05893cceb4a0a3a
kjohn01 <r01631002@ntu.edu.tw>
2018-12-27 17:49:02 +0800

    taking input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        };
+        this.addReminder = this.addReminder.bind(this);
+    };
+
+    addReminder() {
+        console.log('this.state', this.state);
+    }
+
     render() {
         return(
             <div className="App">
                 <div className="title">Reminder</div>
                 <div className="form-inline">
                     <div className="form-group">
-                        <input className="form-control" placeholder="I have to ..."/>
+                        <input 
+                            className="form-control" 
+                            placeholder="I have to ..."
+                            onChange={event => this.setState({ text: event.target.value})}
+                        />
                     </div>
-                    <button className="btn btn-success" type="button">
+                    <button 
+                        className="btn btn-success" 
+                        type="button"
+                        onClick={() => this.addReminder()}
+                    >
                         Add
                     </button>
                 </div>
