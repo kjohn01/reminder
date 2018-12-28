0d3154c5fc6607f48910efd37440779cc03ef94c
kjohn01 <r01631002@ntu.edu.tw>
2018-12-28 16:43:20 +0800

    manupilating cookies

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+import { bake_cookie, read_cookie } from 'sfcookies';
 import { ADD_REMINDER, DELETE_REMINDER, CLEAR_REMINDERS } from '../constants';
 
 const reminder = (action) => {
@@ -10,23 +11,24 @@ const reminder = (action) => {
 
 const removeById = (state, id) => {
     const reminders = state.filter(reminder => reminder.id !== id);
-    console.log('new reduced reminders', reminders);
     return reminders;
   }
 
 const reducer = (state = [], action) => {
     let reminders = null;
+    state = read_cookie('reminders');
     switch (action.type) {
         case ADD_REMINDER:
             reminders = [...state, reminder(action)];
-            console.log('reminders as state', reminders);
+            bake_cookie('reminders', reminders);
             return reminders;
         case DELETE_REMINDER:
             reminders = removeById(state, action.id);
-            console.log('reminders as state', reminders);
+            bake_cookie('reminders', reminders);
             return reminders;
         case CLEAR_REMINDERS:
             reminders = [];
+            bake_cookie('reminders', reminders);
             return reminders;
         default:
             return state;
